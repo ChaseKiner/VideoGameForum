e4a63b9e2f59f660f8a5b15876ea205876af1dbd
ChaseKiner <chasekiner@hotmail.com>
2017-12-01 22:35:45 -0500

    reformatted some code and added output buffer

--- postReply.inc.php
+++ postReply.inc.php
@@ -1,11 +1,12 @@
 <?php
     include 'connect.php';
+    ob_start();
 
     function displayTopic($connect){
-        $query = 'SELECT * FROM Message WHERE MessageId = '.$_GET['id'];
+        $query = 'SELECT * FROM message WHERE MessageId = '.$_GET['id'];
         $res = mysqli_query($connect, $query);
         $row = mysqli_fetch_assoc($res);
-        $q = 'SELECT FirstName, LastName FROM User WHERE UserId = '.$row["Posts"];
+        $q = 'SELECT FirstName, LastName FROM user WHERE UserId = '.$row["Posts"];
         $name = mysqli_fetch_assoc(mysqli_query($connect, $q));
 
         echo '<div id="topic-post">
@@ -34,7 +35,7 @@
     }
     
     function fetchReply($rowReply, $connect) {
-        $replierQuery = 'SELECT FirstName, LastName FROM User WHERE UserId = '.$rowReply["Sends"];
+        $replierQuery = 'SELECT FirstName, LastName FROM user WHERE UserId = '.$rowReply["Sends"];
         $replierName = mysqli_fetch_assoc(mysqli_query($connect, $replierQuery));
 
         echo '<tr>';
@@ -48,7 +49,7 @@
 
     ///Query replies for the id in $_GET['id']
     function queryReplies($connect) {
-        $repliesQuery = 'SELECT Content, Sends FROM Reply WHERE Attached = '.$_GET['id'];
+        $repliesQuery = 'SELECT Content, Sends FROM reply WHERE Attached = '.$_GET['id'];
         $resReply = mysqli_query($connect, $repliesQuery);
         return $resReply;
     }
@@ -66,25 +67,29 @@
 
     function insertReplyForm($connect) {
         //Reply form
-        if(isset($_SESSION["userId"])){
+        
+        if(isset($_SESSION["userId"])) { 
+        if($_SERVER['REQUEST_METHOD'] != 'POST') {
             echo "<form method='post' action=''>
                 Reply: <br><textarea name='reply' /></textarea><br>
                 <input class = 'item' type='submit' value='reply' />
             </form>";
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        }
+        else {
             //check if reply has any content
             if($_POST['reply'] != ''){
-            $sql = "INSERT INTO Reply(Content, Sends, Attached, DatePosted)
-            VALUES('". mysqli_real_escape_string($_POST['reply']) ."',
+            $sql = "INSERT INTO reply(Content, Sends, Attached, DatePosted)
+            VALUES('". mysqli_real_escape_string($connect, $_POST['reply']) ."',
                     ".$_SESSION["userId"].",
                     ".$_GET["id"].",
                     NOW())";
             $result = mysqli_query($connect, $sql);
-            }
+            
         //display the new reply
         $url = 'topic.php?id='.$_GET["id"];
         header("Location: $url");
-}
-}
+            }
+        }
     }
+}
 ?>
