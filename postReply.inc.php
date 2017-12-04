ad64ee4137a8acbf2b0974495077f0a1feb151c1
Mikyla R Wilfred <mikyla.wilfred@gmail.com>
2017-12-03 21:09:36 -0500

    Update postReply.inc.php
    Added email for a thread

--- postReply.inc.php
+++ postReply.inc.php
@@ -113,6 +113,47 @@
                     ".$_GET["id"].",
                     NOW())";
             $result = mysqli_query($connect, $sql);
+		    
+		    
+		    
+		    
+	$query = "SELECT * from favorite where ParentTable = 'Topic' and FavoritedId = $_GET['id']";
+        $result = mysqli_query($connect, $query);
+        while($row = mysqli_fetch_assoc($result)) {
+           $query_2 = "Select * from user where $row['UserWhoFavorited'] = UserId";
+            $result_2 = mysqli_query($connect, $query_2);
+            $row_email = mysqli_fetch_assoc($result_2);
+            
+            $to = $row_email['Email'];
+				
+				//who sends the email
+				$from = $row_email['Email'];
+				
+				//reply-to test
+				$reply_to = $row_email['Email'];
+					
+				$subject = "New reply on topic you favorited ";
+				
+				//begin of HTML message 
+				$body = "<html>
+						<body>
+		                    http://ec2-35-160-100-189.us-west-2.compute.amazonaws.com/topic.php?id=".$_GET['id'] . "
+						</body>
+						</html>";
+				//end of message 
+				
+				// To send the HTML mail we need to set the Content-type header. 
+				$headers = "From: " . strip_tags($from) . "\r\n";
+				$headers .= "Reply-To: ". strip_tags($reply_to) . "\r\n";
+				$headers .= "MIME-Version: 1.0\r\n";
+				$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+				
+				//send the email - not yet
+				mail("$to","$subject","$body","$headers");
+            
+        }
+		    
+		    
             
         //display the new reply
         $url = 'topic.php?id='.$_GET["id"];
