73c049c03e5269e60be0653e027bcdba246b9349
Chase Kiner <chasekiner@hotmail.com>
2017-12-04 10:18:51 -0500

    changed query slightly and fixed create topic button

--- category_search.php
+++ category_search.php
@@ -4,7 +4,7 @@
     
     /// Query for retrieving topics
     include 'postTopic.inc.php';
-        $q = "SELECT Title, MessageId, Posts FROM message WHERE Category = ".$_GET['category']." and archive = 0 and (Content LIKE '%". $_GET['SValue'] .'%" or Title LIKE "%'. $_GET['SValue'] .'%") ORDER BY';
+        $q = "SELECT Title, MessageId, Posts FROM message WHERE Category = ".$_GET['category']." and (Content LIKE '%". $_GET['SValue'] ."%' or Title LIKE '%". $_GET['SValue'] ."%')";
     $r = mysqli_query($connect, $q);
     
     /// Begin table creation
@@ -15,6 +15,6 @@
     }
     echo "</table>";
     echo"<br>";
-    echo '<a class="item" href="create_topic.php?id='.$_GET['id'].'">Create a topic</a>';
-    include 'footer.php'
+    echo '<a class="item" href="create_topic.php?id='.$_GET['category'].'">Create a topic</a>';
+    include 'footer.php';
 ?>
