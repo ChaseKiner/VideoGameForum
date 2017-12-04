fde91e7b67eb5d4cda454eb42b5e6aaed7853841
Mikyla R Wilfred <mikyla.wilfred@gmail.com>
2017-12-04 08:29:15 -0500

    Update quiz.php
    Minor adjustments like a submit button

--- quiz.php
+++ quiz.php
@@ -9,7 +9,7 @@
 <form role="form" action="quiz_results.php" method="post">
 <table>
 <tr>
-<?php 
+	<td> Franchise </td> <td><?php 
              	$rs_q = "SELECT DISTINCT franchise FROM videogames ORDER BY franchise ASC";
          		$rs_rs = mysqli_query($connect, $rs_q);
 				
@@ -24,27 +24,23 @@
 				echo '</select>';
 
             ?>
-
+	</td> 
 </tr>
-<tr>
+<tr><td> Rating </td> <td>
 <?php 
-             	$rs_q = "SELECT DISTINCT rating FROM videogames ORDER BY rating ASC";
-         		$rs_rs = mysqli_query($connect, $rs_q);
-				
-				//start the drop down box
-				echo '<select id="rating" name="rating">';
-				
-				while($rs_row = mysqli_fetch_array($rs_rs))	{
-					echo '<option value="'.$rs_row['rating'].'">'.$rs_row['rating'].'</option>\n';
-				}
-				
-				//end the dropdown box
-				echo '</select>';
+             	echo '<select id="rating" name="rating">';
+			echo '<option value="50">50-59</option>\n';
+echo '<option value="60">60-69</option>\n';
+echo '<option value="70">70-79</option>\n';
+echo '<option value="80">80-89</option>\n';
+echo '<option value="90">90-100</option>\n';
+			
+		echo '</select>';
 
             ?>
-
+	</td>
 </tr>
-<tr>
+<tr><td> Game Modes </td> <td>
 <?php 
              	$rs_q = "SELECT DISTINCT game_modes FROM videogames ORDER BY game_modes ASC";
          		$rs_rs = mysqli_query($connect, $rs_q);
@@ -60,9 +56,9 @@
 				echo '</select>';
 
             ?>
-
+	</td>
 </tr>
-<tr>
+<tr><td> Genre </td> <td>
 <?php 
              	$rs_q = "SELECT  DISTINCT genres FROM videogames ORDER BY genres ASC";
          		$rs_rs = mysqli_query($connect, $rs_q);
@@ -78,9 +74,9 @@
 				echo '</select>';
 
             ?>
-
+	</td>
 </tr>
-<tr>
+<tr><td> Popularity </td> <td>
 <?php 
              	$rs_q = "SELECT DISTINCT popularity FROM videogames ORDER BY popularity ASC";
          		$rs_rs = mysqli_query($connect, $rs_q);
@@ -101,7 +97,7 @@
 
 
 
-
+	<tr> <td> </td> <td> <input type="submit" value="Submit"> </td> 
 </table>
 </form> 
 <?php
